7b866cb9d25d477dfc8c890a1b64c524d36bce0a
Armelle <armelle.lecerf@orange.fr>
2020-01-20 09:05:35 +0100

    ajout de broadSword

---
+++ COMBAT ANTIQUE/BroadSword.swift
@@ -0,0 +1,14 @@
+//
+//  BroadSword.swift
+//  COMBAT ANTIQUE
+//
+//  Created by macmini-Armelle on 20/01/2020.
+//  Copyright © 2020 armellelecerf. All rights reserved.
+//
+
+import Foundation
+class BroadSword : Arm {
+    init() {
+        super.init (damage : 18)
+    }
+}
