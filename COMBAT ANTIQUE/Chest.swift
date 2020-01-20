6d614642f0d99302c388bad4143ea459c777cd0d
Armelle <armelle.lecerf@orange.fr>
2020-01-20 08:52:30 +0100

    ajout de Chest

---
+++ COMBAT ANTIQUE/Chest.swift
@@ -0,0 +1,35 @@
+//
+//  Chest.swift
+//  projet3 essais
+//
+//  Created by macmini-Armelle on 07/12/2019.
+//  Copyright © 2019 armellelecerf. All rights reserved.
+//
+
+import Foundation
+class Chest{
+    var randomStrenght : Int
+    init(randomStrenght : Int)
+    {self.randomStrenght = randomStrenght}
+    func appear()->Bool{
+        let random = Int.random(in: 1..<7)
+        if random % 2 == 0 {
+            print("A chest has appeared\n")
+        return true
+        }
+        else {return false}
+    }
+    func armStrenght()->Int{
+        let randomInteger = Int.random(in: 8..<21)
+        self.randomStrenght = randomInteger
+        
+        
+        return randomInteger
+    }
+    func exchange()->Int{
+        var damage : Int = 10
+         damage = self.armStrenght()
+    return damage
+    
+    }
+}
