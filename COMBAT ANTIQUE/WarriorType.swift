7ece936ad9449de7a05fbb89879feda86d6886cd
Armelle <armelle.lecerf@orange.fr>
2020-01-20 09:10:34 +0100

    Ajout de WarriorType

---
+++ COMBAT ANTIQUE/WarriorType.swift
@@ -0,0 +1,13 @@
+//
+//  WarriorType.swift
+//  COMBAT ANTIQUE
+//
+//  Created by macmini-Armelle on 20/01/2020.
+//  Copyright © 2020 armellelecerf. All rights reserved.
+//
+
+import Foundation
+
+enum WarriorType {
+case viking, gladiator, knight
+}
