a6d9943c414a0f431cf65286a82a604ccf3ba21d
Armelle <armelle.lecerf@orange.fr>
2020-01-20 09:07:50 +0100

    ajout de RandomArm

---
+++ COMBAT ANTIQUE/RandomArm.swift
@@ -0,0 +1,14 @@
+//
+//  RandomArm.swift
+//  COMBAT ANTIQUE
+//
+//  Created by macmini-Armelle on 20/01/2020.
+//  Copyright © 2020 armellelecerf. All rights reserved.
+//
+
+import Foundation
+class RandomArm : Arm {
+    init() {
+        super.init (damage : 15)
+    }
+}
